ddd6c1f2cff42cfed412b07c992ff531a7a1a2f8
Joseph Hsu <jhsu.x1@gmail.com>
2013-02-25 12:25:58 -0800

    basic test

--- amok.js
+++ amok.js
@@ -1,64 +1,23 @@
 var ar = require('ar-drone');
-var drone = ar.createUdpControl();
+var client = ar.createClient();
 
-var ref = { emergency : true };
-var pcmd = {};
+// var time = require('./decidedTime');
 
-setTimeout(function () {
-  ref.emergency = false;
-  ref.fly = true;
-  setTimeout(amok, 4000);
-}, 1000);
+doSomething();
 
-setTimeout(function () {
-  ref.fly = false;
-}, 30 * 1000);
+function doSomething() {
+  client.takeoff();
+  client
+    .after(8000, function() {
+      client.land();
+    });
+}
 
-setInterval(function () {
-  drone.ref(ref);
-  drone.pcmd(pcmd);
-  drone.flush();
-}, 30);
-
-function amok () {
-  var iv = setInterval(function () {
-    pcmd = {};
-    ref = { fly : true };
-    var actions = [
-      function () { up(Math.random() * 2 - 1) },
-      function () { left(Math.random() * 2 - 1) },
-      function () { turn(Math.random() * 2 - 1) },
-    ];
-    actions[Math.floor(Math.random() * actions.length)]();
-  }, 1000);
-  
-  setTimeout(function () {
-    ref.fly = false;
-  }, 10 * 1000);
+function amok() {
+  // now = new Date();
+  // while(now < time.decidedTime) {
+  //   blink();
+  //   now = Date();
+  // }
+  doSomething();
 }  
-
-function takeOff () {
-  ref.fly = true;
-  ref.emergency = false;
-  drone.flush();
-}
-
-function land () {
-  ref.fly = false;
-}
-
-function front (v) {
-  pcmd.front = v;
-}
-
-function left (v) {
-  pcmd.left = v;
-}
-
-function up (v) {
-  pcmd.up = v;
-}
-
-function turn (v) {
-  pcmd.clockwise = v;
-}
